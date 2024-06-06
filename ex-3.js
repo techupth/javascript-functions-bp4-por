e27877b91c61934b70e528405f58cba4c74fc9f6
Por <Poramaporn2540@gmail.com>
2024-06-06 15:32:53 +0700

    feat(exercise): complete exercises

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,25 @@
 // Start coding here
+// Start coding here
+//declaration
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+//Stores
+const allFunction = [
+    {func: add, name: 'add'},
+    {func: subtract, name: 'subtract'},
+    {func: multiply, name: 'multiply'},
+    {func: divide, name: 'divide'},
+];
+
+//Sample arguements
+const a = 10;
+const b = 5;
+
+//Iterate
+allFunction.forEach(({func, name}) => {
+    console.log(func(a,b))
+})
+
